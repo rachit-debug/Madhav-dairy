65aea47f44bcf261c35e4ed477735fde23d58dfa
rachit-debug <thapliyalrachit8@gmail.com>
2025-03-28 15:39:01 +0530

    Update urls.py

--- led/urls.py
+++ led/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-path('admin/', admin.site.urls),
+path('local/', admin.site.urls),
 path('',include('mainpage.url')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
